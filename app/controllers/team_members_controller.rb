2b055e118a96719b2095996e55562b91c30d22e1
rgabriel01 <raymond.m.gabriel@gmail.com>
2024-12-15 20:18:16 +0800

    FEATURE | OAUTH implementation with scopes

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,4 +1,8 @@
 class TeamMembersController < ApplicationController
+  before_action only: :index do
+    doorkeeper_authorize! :admin
+  end
+
   def index
     team_member = TeamMember.all
     render json: team_member, status: :ok
